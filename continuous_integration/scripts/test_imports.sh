899282b92a1e1cbba6c69733365ee39b582d9582
James Bourbeau <jrbourbeau@gmail.com>
2021-03-06 14:40:35 -0600

    test_imports.sh

--- continuous_integration/scripts/test_imports.sh
+++ continuous_integration/scripts/test_imports.sh
@@ -14,12 +14,8 @@ test_import () {
     conda env remove -n test-imports
 }
 
-# Note: in setup.py, bag and delayed require cloudpickle, but it's omitted here as it is
-# only a dependency for real-life usage and unit tests
-test_import ""                                "import dask, dask.multiprocessing, dask.threaded, dask.optimization"
-test_import "toolz"                           "import dask.base, dask.delayed, dask.graph_manipulation"
-test_import "fsspec toolz partd"              "import dask.bag"
-test_import "numpy toolz"                     "import dask.array"
-test_import "fsspec numpy pandas toolz partd" "import dask.dataframe"
-test_import "bokeh"                           "import dask.diagnostics"
-test_import "distributed"                     "import dask.distributed"
+test_import ""             "import dask, dask.multiprocessing, dask.threaded, dask.optimization, dask.base, dask.delayed, dask.graph_manipulation, dask.bag"
+test_import "numpy"        "import dask.array"
+test_import "numpy pandas" "import dask.dataframe"
+test_import "bokeh"        "import dask.diagnostics"
+test_import "distributed"  "import dask.distributed"
