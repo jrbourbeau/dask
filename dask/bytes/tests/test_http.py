7a031927fa276179da4779b85e2bef6798d2fa75
James Bourbeau <jrbourbeau@gmail.com>
2021-03-06 14:47:10 -0600

    fsspec

--- dask/bytes/tests/test_http.py
+++ dask/bytes/tests/test_http.py
@@ -190,10 +190,6 @@ def test_bag():
     b.compute()
 
 
-@pytest.mark.xfail(
-    LooseVersion(fsspec.__version__) <= "0.4.1",
-    reason="https://github.com/dask/dask/pull/5231",
-)
 @pytest.mark.network
 def test_read_csv():
     dd = pytest.importorskip("dask.dataframe")
