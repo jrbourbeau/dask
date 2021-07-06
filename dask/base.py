9aa2e71dc67715168e178d4a6576c1d5a00ea11b
James Bourbeau <jrbourbeau@gmail.com>
2021-07-06 13:40:45 -0500

    Update pickle protocol in function tokenization

--- dask/base.py
+++ dask/base.py
@@ -894,7 +894,7 @@ def _normalize_function(func):
         return (normalize_function(func.func), args, kws)
     else:
         try:
-            result = pickle.dumps(func, protocol=0)
+            result = pickle.dumps(func, protocol=4)
             if b"__main__" not in result:  # abort on dynamic functions
                 return result
         except Exception:
@@ -902,7 +902,7 @@ def _normalize_function(func):
         try:
             import cloudpickle
 
-            return cloudpickle.dumps(func, protocol=0)
+            return cloudpickle.dumps(func, protocol=4)
         except Exception:
             return str(func)
 
