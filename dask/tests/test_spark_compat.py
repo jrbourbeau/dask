9c85a6a8263c9c5db3710cebef9797163c2ae02d
James Bourbeau <jrbourbeau@gmail.com>
2022-12-05 13:53:02 -0600

    More

--- dask/tests/test_spark_compat.py
+++ dask/tests/test_spark_compat.py
@@ -195,6 +195,10 @@ def test_read_decimal_dtype(spark_session, tmpdir):
 
     # types_mapper = {pa_decimal_type: pd.ArrowDtype(pa_decimal_type)}
     ddf = dd.read_parquet(tmpdir, engine="pyarrow", use_nullable_dtypes="pyarrow")
-    assert ddf.e.dtype == "decimal128(7, 3)[pyarrow]", ddf.e.dtype
-    assert ddf.e.compute().dtype == "decimal128(7, 3)[pyarrow]", ddf.e.compute().dtype
-    assert_eq(ddf, sdf.toPandas(), check_index=False)
+    assert isinstance(ddf.e.dtype.type, pa.Decimal128Type), ddf.e.dtype.type
+    assert isinstance(
+        ddf.e.compute().dtype.type, pa.Decimal128Type
+    ), ddf.e.compute().dtype.type
+    # assert ddf.e.dtype == "decimal128(7, 3)[pyarrow]", ddf.e.dtype
+    # assert ddf.e.compute().dtype == "decimal128(7, 3)[pyarrow]", ddf.e.compute().dtype
+    assert_eq(ddf, pdf, check_index=False)
